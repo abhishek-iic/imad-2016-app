8175a24dfdfacca1484550b17e9bee54230e736f
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-09-28 17:46:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move the image
+var img = document.getElementById('img');
+img.onclick = function (){
+    img.style.marginLeft = '100px';
+    
+};
