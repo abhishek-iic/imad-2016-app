c1761959f58b7d273a29e852b03b3f24ccbd1d56
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-03 14:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,23 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
- //Make a request to the counter endpoint 
+ //Create a request  
+ var request = new XMLHttpRequest();
  
  //capture the response and store it in a variable
- 
-//render the variable in the correct span
-counter = counter+1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+ request.onreadystatechange = function(){
+     if(request.readyState === XMLHttpRequest.DONE){
+         //take some action
+         if(request.status === 200){
+             var counter = request.responseText;
+             var span = getElementById('count');
+             span.innerHTML=counter.toString();
+         }
+     }
+     //not done
+ };
+//make the request
+request.open('GET','http://abhishek-iic.imad.hasura-app.io/counter',true);
+request.send(null);
 };
