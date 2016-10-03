86dfdaa86d75f3b2bb9ae00a96b4aadd856cc458
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-03 13:22:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,12 @@
-console.log('Loaded!');
-
-//change text of main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'New value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function (){
-    var interval = setInterval(moveRight, 50)
-//    img.style.marginLeft = '100px';
-    
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+ //Make a request to the counter endpoint 
+ 
+ //capture the response and store it in a variable
+ 
+//render the variable in the correct span
+counter = counter+1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 };
