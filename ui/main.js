fc20159cd031e0950668ebad3d276c6aac12ca69
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-07 11:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ button.onclick = function(){
 request.open('GET','http://abhishek-iic.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onClick = function(){
+    //Make a request to the server and send the name
+    //capture the list of name and render it  as a list
+    var name = ['name1', 'name2', 'name3'];
+    var list = '';
+    for(var i=0;i<name.lenght;i++){
+        list += '<li>' +name[i] + '<li>';
+    }
+    var ul = document.getElementByid('name_list');
+    ul.innerHTML = list;
+};
