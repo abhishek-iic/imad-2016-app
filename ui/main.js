2c2dfafb3561ce105a5134fcba7dd4ef5719c648
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-07 16:15:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function(){
     var name = ['name1', 'name2', 'name3','name4'];
     var list = '';
     for(var i=0;i<name.lenght;i++){
-        list += '<li>' + name[i] + '<li>';
+        list += '<li>' + name[i] + '</li>';
     }
     var ul = document.getElementById('name_list');
     ul.innerHTML = list;
