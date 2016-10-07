f2ffb6603ffa4dbf3dfbdccd4e7d73160eb719e2
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-07 16:22:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,10 +28,11 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to the server and send the name
+   
     //capture the list of name and render it  as a list
     var names = ['name1', 'name2', 'name3','name4'];
     var list = '';
-    for(var i=0;i<names.lenght;i++){
+    for(var i=0; i < names.lenght; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
