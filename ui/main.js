71e9f853649ff28efd42deb64b1393595c7a857d
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-13 11:42:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,14 +27,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to the server and send the name
-   
+   //Create a request  
+ var request = new XMLHttpRequest();
+ 
+ //capture the response and store it in a variable
+ request.onreadystatechange = function(){
+     if(request.readyState === XMLHttpRequest.DONE){
+         //take some action
+         if(request.status === 200){
     //capture the list of name and render it  as a list
-    var names = ['name1', 'name2', 'name3','name4'];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for(var i=0; i < names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+         }
+     }
+     //not done
+ };
+   
+    //Make a request to the server and send the name
+   //make the request
+request.open('GET','http://abhishek-iic.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
 };
