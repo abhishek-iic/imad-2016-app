c42e5e7d553a270b98d9b96367dcaa8a2398a8ec
abhishek-iic <abhishek.kumar@iic.ac.in>
2016-10-13 11:52:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ request.send(null);
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
    //Create a request  
@@ -51,6 +50,8 @@ submit.onclick = function(){
    
     //Make a request to the server and send the name
    //make the request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 request.open('GET','http://abhishek-iic.imad.hasura-app.io/submit-name?name=' + name,true);
 request.send(null);
 };
